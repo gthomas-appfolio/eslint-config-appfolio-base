df0252ade7b92459c2a2fd9a4f4371bd6e1f2306
Gary Thomas <gary.thomas@appfolio.com>
2017-10-23 16:36:20 -0700

    gt - Update rules
    Correct max-len length
    Soften no-return-assign to warning
    Disable object-curly-newline

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ module.exports = {
     'comma-dangle': 0,
     'import/extensions': 0,
     'import/no-unresolved': 1, // To minimize false errors with Webpack loaders
-    'max-len': [1, { ignoreTrailingComments: true }],
+    'max-len': [1, 100, { ignoreTrailingComments: true }],
     'new-cap': 1,
     'no-console': 1,
     'no-param-reassign': 0,
+    'no-return-assign': 1,
+    'object-curly-newline': 0,
     'spaced-comment': 0
   }
 }
